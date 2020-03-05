4e7e48ea539559f53688c34732e156401af75fbd
kiru883 <drazhnik@inbox.ru>
2020-03-05 14:45:13 +0300

    design / heroku

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from pipeline.pipeline import Model
+import os
 
 
 app = Flask(__name__)
@@ -37,5 +38,6 @@ def get_image():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
